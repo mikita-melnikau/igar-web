106d45c518a225c467db1f49740e4e5fad0d9ab6
Mikita <usesa@yandex.com>
2026-03-20 07:55:26 +0100

    getBaseUrl - small update to prevent source url usage

--- app/helpers/request.helpers.ts
+++ app/helpers/request.helpers.ts
@@ -1,17 +1,17 @@
 import { headers } from "next/headers";
+import { config } from "@/app/config";
 
 export async function getSsrBaseUrl() {
-  if (typeof window !== "undefined") {
-    console.log("11111", window.location.origin);
-    return window.location.origin;
+  if (typeof window === "undefined") {
+    const h = await headers();
+    const proto = h.get("x-forwarded-proto") ?? "http";
+    const forwardedHost = h.get("x-forwarded-host");
+    const host = forwardedHost ?? h.get("host");
+    if (!host) {
+      throw new Error("Cannot determine host");
+    }
+    return `${proto}://${host}`;
   }
-  const h = await headers();
-  const proto = h.get("x-forwarded-proto") ?? "http";
-  const forwardedHost = h.get("x-forwarded-host");
-  const host = forwardedHost ?? h.get("host");
-  if (!host) {
-    throw new Error("Cannot determine host");
-  }
-  console.log("11111", `${proto}://${host}`);
-  return `${proto}://${host}`;
+  const origin = window.location.origin;
+  return origin === config.SOURCE_WEBSITE ? "" : origin;
 }
