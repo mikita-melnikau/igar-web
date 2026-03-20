fae253d83028edf467ea839cc532d158e3cdabf6
Mikita <usesa@yandex.com>
2026-03-20 09:24:59 +0100

    Add fetch revalidation frequency

--- app/components/PageRenderer.tsx
+++ app/components/PageRenderer.tsx
@@ -9,9 +9,12 @@ type Props = {
   searchParams: Promise<{ [key: string]: string | string[] | undefined }>;
 };
 
+const isDev = process.env.NODE_ENV !== "production";
+const revalidationFrequency = isDev ? 30 : 3600;
+
 const fetchPageData = async (pathToFetch: string): Promise<ContentResponse> => {
   const body = JSON.stringify({ path: pathToFetch });
-  const options = { method: "PUT", body };
+  const options = { method: "PUT", body, next: { revalidate: revalidationFrequency } };
   const baseUrl = await getSsrBaseUrl();
   const response = await fetch(`${baseUrl}/api/content`, options);
   return response.json();
